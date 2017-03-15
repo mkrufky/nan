682e466efd18b54528c8aff4545d2afb0e0349b9
Michael Ira Krufky <mkrufky@gmail.com>
2017-03-15 11:14:47 -0400

    nan_json.h: we *always* need a HandleScope
    Signed-off-by: Michael Ira Krufky <mkrufky@gmail.com>

--- nan_json.h
+++ nan_json.h
@@ -63,8 +63,8 @@ class JSON {
 
   inline
   Nan::MaybeLocal<v8::Value> Parse(v8::Local<v8::String> jsonString) {
-#if NAN_JSON_H_NEED_PARSE
     Nan::HandleScope scope;
+#if NAN_JSON_H_NEED_PARSE
     return parse(jsonString);
 #else
 #if (NODE_MAJOR_VERSION >= 7)
@@ -77,8 +77,8 @@ class JSON {
 
   inline
   Nan::MaybeLocal<v8::String> Stringify(v8::Local<v8::Object> jsonObject) {
-#if NAN_JSON_H_NEED_STRINGIFY
     Nan::HandleScope scope;
+#if NAN_JSON_H_NEED_STRINGIFY
     return stringify(jsonObject)->ToString();
 #else
     return v8::JSON::Stringify(Nan::GetCurrentContext(), jsonObject);
@@ -88,8 +88,8 @@ class JSON {
   inline
   Nan::MaybeLocal<v8::String> Stringify(v8::Local<v8::Object> jsonObject,
     v8::Local<v8::String> gap) {
-#if NAN_JSON_H_NEED_STRINGIFY
     Nan::HandleScope scope;
+#if NAN_JSON_H_NEED_STRINGIFY
     return stringify(jsonObject, gap)->ToString();
 #else
     return v8::JSON::Stringify(Nan::GetCurrentContext(), jsonObject, gap);
