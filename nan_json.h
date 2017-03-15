4cc8ff63f7e246fee7602e7ce1d6b12a8bf0c20d
Michael Ira Krufky <mkrufky@gmail.com>
2017-03-15 11:38:27 -0400

    Revert "nan_json.h: use Nan::Get rather than v8::Object::Get"
    This reverts commit 79a14095935dc33162d19ec84daa8f1a2f66cd00.

--- nan_json.h
+++ nan_json.h
@@ -66,10 +66,9 @@ class JSON {
   static v8::Local<v8::Value> Call(const char *method,
     int argc, v8::Local<v8::Value> *argv) {
     v8::Local<v8::Value> globalJSON =
-      Nan::Get(
-        Nan::GetCurrentContext()->Global(),
+      Nan::GetCurrentContext()->Global()->Get(
         Nan::New("JSON").ToLocalChecked()
-      ).ToLocalChecked();
+      );
 
     if (!globalJSON->IsObject()) {
       return Nan::Undefined();
@@ -78,7 +77,7 @@ class JSON {
     v8::Local<v8::Object> json = globalJSON->ToObject();
 
     v8::Local<v8::Value> thisMethod =
-      Nan::Get(json, Nan::New(method).ToLocalChecked()).ToLocalChecked();
+      json->Get(Nan::New(method).ToLocalChecked());
 
     if (thisMethod.IsEmpty() || !thisMethod->IsFunction()) {
       return Nan::Undefined();
